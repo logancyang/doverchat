c594937d143b1068894e8b7d6147c716a7f6e32d
Logan Yang <logancyang@gmail.com>
2020-07-12 02:00:35 -0400

    Use flake8 and fix format

--- doverchat/application.py
+++ doverchat/application.py
@@ -1,11 +1,9 @@
 import json
 import logging
-import os
 import time
 import functools
 from contextlib import contextmanager
 
-import flask
 from flask import Flask, flash, render_template, request, redirect, url_for
 from flask_login import current_user, LoginManager, login_required, \
     login_user, logout_user
@@ -14,7 +12,7 @@ from flask_socketio import SocketIO, emit, disconnect, join_room, \
 from sqlalchemy.sql import text
 
 from .db_client import Database
-from .models import LoginUser, User, Message, Room
+from .models import LoginUser, User, Room
 from .query import query_user, query_rooms, query_users
 from .settings import SECRET_KEY
 
@@ -31,9 +29,11 @@ login_manager.init_app(app)
 
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+
 """
 helpers
 """
+
 
 @contextmanager
 def session_scope():
@@ -50,6 +50,7 @@ def session_scope():
     finally:
         session.close()
 
+
 def _get_room_map():
     with session_scope() as session:
         rooms_q = query_rooms()
@@ -58,6 +59,7 @@ def _get_room_map():
         for room_obj in room_objs:
             room_map[room_obj.room_code] = room_obj.room_screen_name
         return room_map
+
 
 def _get_room_access_list(username):
     """Get the room screen names for this user"""
@@ -69,11 +71,13 @@ def _get_room_access_list(username):
         room_map = _get_room_map()
         return [room_map[room_code] for room_code in room_codes]
 
+
 def _get_user_screen_name(username):
     with session_scope() as session:
         q = query_user(username)
         user_obj = session.query(User).from_statement(text(q)).first()
         return user_obj.user_screen_name
+
 
 def _get_all_users():
     """Get all user"""
@@ -83,6 +87,7 @@ def _get_all_users():
             .from_statement(text(user_q)).all()
         return [user_obj.username for user_obj in user_list]
 
+
 def _get_user_creds(username):
     """Get the credentials for this user"""
     with session_scope() as session:
@@ -91,9 +96,11 @@ def _get_user_creds(username):
             .from_statement(text(user_q)).first()
         return user_obj.password
 
+
 """
 Login
 """
+
 
 @login_manager.user_loader
 def user_loader(username):
@@ -105,6 +112,7 @@ def user_loader(username):
     userlogin = LoginUser(username, password)
     return userlogin
 
+
 @login_manager.request_loader
 def request_loader(request):
     username = request.form.get('username')
@@ -114,8 +122,9 @@ def request_loader(request):
 
     password = _get_user_creds(username)
     userlogin = LoginUser(username, password)
-    user.is_authenticated = (request.form['password'] == password)
+    userlogin.is_authenticated = (request.form['password'] == password)
     return userlogin
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -126,7 +135,10 @@ def login():
         username = request.form['username']
         valid_users = _get_all_users()
         if username not in valid_users:
-            logger.info('Client entered wrong username! Attemped username: %s, password: %s' % (username, request.form['password']))
+            logger.info(
+                "Client entered wrong username! Attemped username: %s,"
+                "password: %s" % (username, request.form['password'])
+            )
             flash("错误的用户名或密码")
             return unauthorized_handler()
         password = _get_user_creds(username)
@@ -136,9 +148,12 @@ def login():
             logger.info('Successfully logged in, user: %s', username)
             return redirect(url_for('index'))
 
-        logger.info('Client entered wrong password! Attemped username: %s, password: %s' % (username, request.form['password']))
+        logger.info(
+            "Client entered wrong password! Attemped username: %s,"
+            "password: %s" % (username, request.form['password']))
         flash("错误的用户名或密码")
         return unauthorized_handler()
+
 
 @app.route('/logout')
 @login_required
@@ -147,18 +162,22 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return redirect(url_for('login'))
+
 
 """
 Views and sockets
 """
 
+
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
+
 
 def authenticated_only(f):
     @functools.wraps(f)
@@ -168,6 +187,7 @@ def authenticated_only(f):
         else:
             return f(*args, **kwargs)
     return wrapped
+
 
 @app.route('/userrooms')
 @authenticated_only
@@ -185,6 +205,7 @@ def get_user_rooms():
             "Current user is not authenticated: %s" % current_user.get_id())
         return False
 
+
 @socketio.on('broadcast_event')
 @authenticated_only
 def chat_broadcast(msg):
@@ -197,9 +218,10 @@ def chat_broadcast(msg):
         'data': msg['data'],
         'room': msg['room']
     }
-    logger.info('broadcast msg_obj %s \nin room %s:'\
-         % (msg_obj, msg['room']))
+    logger.info('broadcast msg_obj %s \nin room %s:'
+                % (msg_obj, msg['room']))
     emit('my_response', msg_obj, room=msg['room'])
+
 
 @socketio.on('join')
 @authenticated_only
@@ -208,7 +230,8 @@ def on_join(msg):
     room = msg['room']
     user_screen_name = _get_user_screen_name(username)
     enter_msg = user_screen_name + '加入了房间: ' + room
-    denied_msg = user_screen_name + ' attempted to join room but denied: ' + room
+    denied_msg = user_screen_name +\
+        ' attempted to join room but denied: ' + room
     curr_time = time.time_ns()//1000000
     msg_obj = {
         'timestamp': f'{curr_time}',
@@ -227,6 +250,7 @@ def on_join(msg):
     join_room(room)
     emit('my_response', msg_obj, room=room)
 
+
 @socketio.on('leave')
 @authenticated_only
 def on_leave(msg):
@@ -234,6 +258,7 @@ def on_leave(msg):
     room = msg['room']
     leave_room(room)
     emit('user_left', username + ' has left the room: ' + room, room=room)
+
 
 @socketio.on('connect')
 @authenticated_only
@@ -248,6 +273,7 @@ def chat_connect():
         logger.error(
             "Current user is not authenticated: %s" % current_user.get_id())
         return False
+
 
 @socketio.on('disconnect')
 @authenticated_only
