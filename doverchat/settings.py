787de3d7f044adf9edcbdff109ee2c1c6332178c
Logan Yang <logancyang@gmail.com>
2020-07-16 15:04:23 -0400

    Migrate to dynamodb and use static files as much as possible for speed

--- doverchat/settings.py
+++ doverchat/settings.py
@@ -5,5 +5,11 @@ from dotenv import load_dotenv
 path = Path(os.path.dirname(__file__))
 parentdir = path.parent
 load_dotenv(parentdir/'.env')
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 POSTGRES_RDS_URL = os.environ.get('POSTGRES_RDS_URL')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.environ.get('AWS_REGION')
