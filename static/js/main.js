03ac18e42f2c011d2c168798c67e1566c824b798
Logan Yang <logancyang@gmail.com>
2020-07-20 18:14:59 -0400

    Fix not working revert

--- static/js/main.js
+++ static/js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
   // Connect to the Socket.IO server.
   // The connection URL has the following format, **relative to the current page**:
   // http[s]://<domain>:<port>[/<namespace>]
-  const socket = io(namespace, {transports: ['websocket']});
+  const socket = io(namespace);
 
   function switchRoom(room_code, room_name) {
     socket.emit('leave', {room_code: CURRENT_ROOM_CODE});
